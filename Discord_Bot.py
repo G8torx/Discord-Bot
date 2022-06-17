da98d1d348aac5ec06e7834e51d7525de1aa08bd
G8torx <104470093+G8torx@users.noreply.github.com>
2022-06-17 11:32:50 -0700

    Discord_Bot.py

--- Discord_Bot.py
+++ Discord_Bot.py
@@ -1,12 +1,15 @@
 import requests
+import time
 
-payload  = {
-    'content' : 'This is a bot'
+message  = {
+    'content' : 'Hi'
 }
 
 header = {
-    'authorization' : 'Nzc4Mjg5NjgyNTA0ODc2MDUy.YjYl_w.npHlQkcGaTQPopCCKBO314FBB6E'
+    'authorization' : 'Nzc4Mjg5NjgyNTA0ODc2MDUy.GDR-GS.Q7NBhOiW-cqwCRfnQ2ObRlFd1yXJoQKZmJfo3w'
 }
 
-for i in range(10):
-    r = requests.post('https://discord.com/api/v9/channels/976934761056567317/messages', data=payload, headers=header)
+#Prints 'Hi' every second 25 times in discord
+for i in range(25):
+    r = requests.post('https://discord.com/api/v9/channels/981272777367625788/messages', data=message, headers=header)
+    time.sleep(1)
